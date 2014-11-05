32d8afead5074d6cd08a6a8718a164688538bb25
intendo <Darren.Curtis@pnnl.gov>
2014-11-05 15:36:17 -0800

    Fixed whitespace issues in Eclipse editor.

--- src/main/java/gov/pnnl/marker/genes/scan/MapReduceScan.java
+++ src/main/java/gov/pnnl/marker/genes/scan/MapReduceScan.java
@@ -41,160 +41,143 @@ import com.beust.jcommander.Parameter;
  */
 public class MapReduceScan extends Configured implements Tool {
 
-	static class Opts extends ClientOnRequiredTable {
-		@Parameter(names = "--output", description = "output file")
-		String outputDirectory;
+    static class Opts extends ClientOnRequiredTable {
+        @Parameter(names = "--output", description = "output file")
+        String outputDirectory;
 
-		@Parameter(names = "--columns", description = "columns to extract, in cf:cq{,cf:cq,...} form")
-		String columns = "";
+        @Parameter(names = "--columns", description = "columns to extract, in cf:cq{,cf:cq,...} form")
+        String columns    = "";
 
-		@Parameter(names = "--gtscore", description = "include sequences with bit score greater than value")
-		String gtscore = null;
+        @Parameter(names = "--gtscore", description = "include sequences with bit score greater than value")
+        String gtscore    = null;
 
-		@Parameter(names = "--gtpctcov", description = "include sequences with alignment percent coverage greater than value")
-		String gtpctcov = null;
+        @Parameter(names = "--gtpctcov", description = "include sequences with alignment percent coverage greater than value")
+        String gtpctcov   = null;
 
-		@Parameter(names = "--gtpostprob", description = "include sequences with posterior probability greater than value")
-		String gtpostprob = null;
-	}
+        @Parameter(names = "--gtpostprob", description = "include sequences with posterior probability greater than value")
+        String gtpostprob = null;
+    }
 
-	public static class MapClass extends Mapper<Key, Value, NullWritable, Text> {
-		@Override
-		public void map(final Key key,
-				Value value,
-final Context output) throws IOException, InterruptedException {
-			//output.write(NullWritable.get(), new Text("INIT"));
-			Text data = new Text("data");
-			//output.write(NullWritable.get(), new Text("CHECK"));
-			SortedMap<Key, Value> decodedRow = WholeRowIterator.decodeRow(key, value);
-		    for (Entry<Key, Value> row : decodedRow.entrySet()) {
-		    	//output.write(NullWritable.get(), new Text("ROW:" + row.getKey() + ":"));
-				//output.write(NullWritable.get(), new Text("VAL:" + row.getValue() + ":"));
-		    	if (0 == row.getKey().compareColumnQualifier(data)) {
-					output.write(NullWritable.get(), new Text(row.getValue()
-							.toString()));
-		    	}
-		    }
-		}
-	}
+    public static class MapClass extends Mapper<Key, Value, NullWritable, Text> {
+        @Override
+        public void map(final Key key, Value value, final Context output) throws IOException, InterruptedException {
+            Text data = new Text("data");
+            SortedMap<Key, Value> decodedRow = WholeRowIterator.decodeRow(key, value);
+            for (Entry<Key, Value> row : decodedRow.entrySet()) {
+                if (0 == row.getKey().compareColumnQualifier(data)) {
+                    output.write(NullWritable.get(), new Text(row.getValue().toString()));
+                }
+            }
+        }
+    }
 
-	public static class FilterBitScore extends WholeRowIterator {
+    public static class FilterBitScore extends WholeRowIterator {
 
-		private Double score = null;
+        private Double score           = null;
 
-		private Double postProb = null;
+        private Double postProb        = null;
 
-		private Double percentCoverage = null;
+        private Double percentCoverage = null;
 
-		@Override
-		public void init(final SortedKeyValueIterator<Key, Value> source,
-				final Map<String, String> options, final IteratorEnvironment env)
-				throws IOException {
-			super.init(source, options, env);
+        @Override
+        public void init(final SortedKeyValueIterator<Key, Value> source, final Map<String, String> options, final IteratorEnvironment env) throws IOException {
+            super.init(source, options, env);
 
-			if (options.containsKey("score")) {
-				score = Double.parseDouble(options.get("score"));
-			}
-			if (options.containsKey("post_prob")) {
-				postProb = Double.parseDouble(options.get("post_prob"));
-			}
-			if (options.containsKey("percent_coverage")) {
-				percentCoverage = Double.parseDouble(options
-						.get("percent_coverage"));
-			}
-		}
+            if (options.containsKey("score")) {
+                score = Double.parseDouble(options.get("score"));
+            }
+            if (options.containsKey("post_prob")) {
+                postProb = Double.parseDouble(options.get("post_prob"));
+            }
+            if (options.containsKey("percent_coverage")) {
+                percentCoverage = Double.parseDouble(options.get("percent_coverage"));
+            }
+        }
 
-		@Override
-		public boolean filter(Text currentRow, List<Key> keys,
-				List<Value> values) {
-			for (int i = 0; i < keys.size(); ++i) {
-				Key k = keys.get(i);
-				Value v = values.get(i);
-				if ((score != null)
-						&& k.getColumnQualifier().toString().equals("score")) {
-					final Double value = Double.parseDouble(v.toString());
-					if (value < score) {
-						return false;
-					}
-				}
-				if ((postProb != null)
-						&& k.getColumnQualifier().toString()
-								.equals("post_prob")) {
-					final Double value = Double.parseDouble(v.toString());
-					if (value < postProb) {
-						return false;
-					}
-				}
-				if ((percentCoverage != null)
-						&& k.getColumnQualifier().toString()
-								.equals("percent_coverage")) {
-					final Double value = Double.parseDouble(v.toString());
-					if (value < percentCoverage) {
-						return false;
-					}
-				}
-			}
-			return true;
-		}
+        @Override
+        public boolean filter(Text currentRow, List<Key> keys, List<Value> values) {
+            for (int i = 0; i < keys.size(); ++i) {
+                Key k = keys.get(i);
+                Value v = values.get(i);
+                if ((score != null) && k.getColumnQualifier().toString().equals("score")) {
+                    final Double value = Double.parseDouble(v.toString());
+                    if (value < score) {
+                        return false;
+                    }
+                }
+                if ((postProb != null) && k.getColumnQualifier().toString().equals("post_prob")) {
+                    final Double value = Double.parseDouble(v.toString());
+                    if (value < postProb) {
+                        return false;
+                    }
+                }
+                if ((percentCoverage != null) && k.getColumnQualifier().toString().equals("percent_coverage")) {
+                    final Double value = Double.parseDouble(v.toString());
+                    if (value < percentCoverage) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
 
-	}
+    }
 
-	@Override
-	public int run(final String[] args) throws Exception {
-		final Opts opts = new Opts();
-		opts.parseArgs(MapReduceScan.class.getName(), args);
+    @Override
+    public int run(final String[] args) throws Exception {
+        final Opts opts = new Opts();
+        opts.parseArgs(MapReduceScan.class.getName(), args);
 
-		final Job job = Job.getInstance(getConf());
-		job.setJobName(MapReduceScan.class.getName());
-		job.setJarByClass(this.getClass());
+        final Job job = Job.getInstance(getConf());
+        job.setJobName(MapReduceScan.class.getName());
+        job.setJarByClass(this.getClass());
 
-		job.setInputFormatClass(AccumuloInputFormat.class);
-		final HashSet<Pair<Text, Text>> columnsToFetch = new HashSet<Pair<Text, Text>>();
-		for (final String col : opts.columns.split(",")) {
-			final int idx = col.indexOf(":");
-			final Text cf = new Text(idx < 0 ? col : col.substring(0, idx));
-			final Text cq = idx < 0 ? null : new Text(col.substring(idx + 1));
-			if (cf.getLength() > 0) {
-				columnsToFetch.add(new Pair<Text, Text>(cf, cq));
-			}
-		}
+        job.setInputFormatClass(AccumuloInputFormat.class);
+        final HashSet<Pair<Text, Text>> columnsToFetch = new HashSet<Pair<Text, Text>>();
+        for (final String col : opts.columns.split(",")) {
+            final int idx = col.indexOf(":");
+            final Text cf = new Text(idx < 0 ? col : col.substring(0, idx));
+            final Text cq = idx < 0 ? null : new Text(col.substring(idx + 1));
+            if (cf.getLength() > 0) {
+                columnsToFetch.add(new Pair<Text, Text>(cf, cq));
+            }
+        }
 
-		if (!columnsToFetch.isEmpty()) {
-			InputFormatBase.fetchColumns(job, columnsToFetch);
-		}
+        if (!columnsToFetch.isEmpty()) {
+            InputFormatBase.fetchColumns(job, columnsToFetch);
+        }
 
-		final Map<String, String> properties = new HashMap<>();
-		if (opts.gtscore != null) {
-			properties.put("score", opts.gtscore);
-		}
-		if (opts.gtpctcov != null) {
-			properties.put("percent_coverage", opts.gtpctcov);
-		}
-		if (opts.gtpostprob != null) {
-			properties.put("post_prob", opts.gtpostprob);
-		}
-		if (!properties.isEmpty()) {
-			final IteratorSetting iteratorSetting = new IteratorSetting(21,
-					"greaterthan", FilterBitScore.class, properties);
-			InputFormatBase.addIterator(job, iteratorSetting);
-			InputFormatBase.setLocalIterators(job, true);
-		}
+        final Map<String, String> properties = new HashMap<>();
+        if (opts.gtscore != null) {
+            properties.put("score", opts.gtscore);
+        }
+        if (opts.gtpctcov != null) {
+            properties.put("percent_coverage", opts.gtpctcov);
+        }
+        if (opts.gtpostprob != null) {
+            properties.put("post_prob", opts.gtpostprob);
+        }
+        if (!properties.isEmpty()) {
+            final IteratorSetting iteratorSetting = new IteratorSetting(21, "greaterthan", FilterBitScore.class, properties);
+            InputFormatBase.addIterator(job, iteratorSetting);
+            InputFormatBase.setLocalIterators(job, true);
+        }
 
-		job.setMapperClass(MapClass.class);
-		job.setNumReduceTasks(0);
+        job.setMapperClass(MapClass.class);
+        job.setNumReduceTasks(0);
 
-		job.setOutputFormatClass(FastaOutputFormat.class);
-		FileOutputFormat.setOutputPath(job, new Path(opts.outputDirectory));
-		job.setOutputKeyClass(NullWritable.class);
-		job.setOutputValueClass(Text.class);
+        job.setOutputFormatClass(FastaOutputFormat.class);
+        FileOutputFormat.setOutputPath(job, new Path(opts.outputDirectory));
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
 
-		opts.setAccumuloConfigs(job);
+        opts.setAccumuloConfigs(job);
 
-		job.waitForCompletion(true);
-		return 0;
-	}
+        job.waitForCompletion(true);
+        return 0;
+    }
 
-	public static void main(final String[] args) throws Exception {
-		ToolRunner.run(new Configuration(), new MapReduceScan(), args);
-	}
+    public static void main(final String[] args) throws Exception {
+        ToolRunner.run(new Configuration(), new MapReduceScan(), args);
+    }
 }
